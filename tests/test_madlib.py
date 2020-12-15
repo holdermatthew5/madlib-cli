afd89468dc9f5abdb449d0515f5d35eb5ec63c81
Matthew Holder <holdermatthew5@gmail.com>
2020-12-14 16:05:35 -0800

    test parse

--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -4,3 +4,19 @@ from madlib_cli.madlib import merge
 
 def test_read_template():
     assert 'I the one and two three have four five\'s six sister and plan to steal her seven eight!' == read_template("madlib_cli/template.txt")
+
+def test_parse_template():
+    words = [
+        'onet',
+        'twot',
+        'threet',
+        'fourt', 
+        'fivet',
+        'sixt',
+        'sevent', 
+        'eightt',
+    ]
+    file = open('madlib_cli/template.txt', 'r')
+    file = file.read()
+    print(parse_template(file, words))
+    assert parse_template(file, words) == 'I the onet and twot threet have fourt fivet\'s sixt sister and plan to steal her sevent eightt!'
