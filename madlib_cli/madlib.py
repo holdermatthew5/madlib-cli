fbd66edd928404ab3bf0ada11ba8815bb4f68a24
Matthew Holder <holdermatthew5@gmail.com>
2020-12-14 16:06:35 -0800

    parse

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -2,3 +2,21 @@ def read_template(path):
     file = open(path, 'r')
     file = file.read()
     return file
+
+def parse_template(template, words):
+    regex = [
+        'one',
+        'two',
+        'three',
+        'four', 
+        'five',
+        'six',
+        'seven', 
+        'eight',
+    ]
+    string = template
+    i = 0
+    while i < len(words):
+        string = re.sub(regex[i], words[i], string)
+        i += 1
+    return string
